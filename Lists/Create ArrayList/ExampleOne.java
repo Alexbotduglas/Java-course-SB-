14e89a9e8f935cb72e3ed392a26388d840fe385b
Alexbotduglas <52333919+Alexbotduglas@users.noreply.github.com>
2025-05-24 18:19:51 +0300

    Create ExampleOne.java

---
+++ Lists/Create ArrayList/ExampleOne.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<String> list =  new ArrayList<>();
+        list.add("First");
+        list.add("Sceond");
+        list.add("Third");
+    }
+}
